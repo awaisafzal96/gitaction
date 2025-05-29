505d6ec23cd0f318d844213d110842dc2a42d0a5
awaisafzal96 <awai5.afz2l@gmail.com>
2025-05-29 15:19:57 +0500

    Update dc.py

--- .github/workflows/dc.py
+++ .github/workflows/dc.py
@@ -1,1 +1,18 @@
+# Printing "Hello, World!"
+print("Hello, World!")
 
+# Using variables
+x = 5
+y = 10
+sum = x + y
+print(sum)
+
+# Conditional statement
+if x > 3:
+  print("x is greater than 3")
+else:
+  print("x is not greater than 3")
+
+# Loop
+for i in range(5):
+  print(i)
